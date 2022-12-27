f1da96cbd6df1972c9df02bc3afc9a4b609f9752
NateDThomas <nate8dakethomas@gmail.com>
2022-12-27 16:15:11 -0500

    Initial setup and testing, made setup instructions

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { StyleSheet, Button, Text, View, ViewPagerAndroid } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button color="rgb(255,0,0)" title="Its always too late"></Button>
-      <Text>HUdiojk</Text>
+      <Button color="rgb(255,0,0)" title="Test"/>
+      <Button color="rgb(0,255,0)" title="Tester"/>
+      <Button color="rgb(255,0,255)" title="Testest"/>
+      <Text>Text</Text>
       <StatusBar style="auto" />
     </View>
   );
